3b5752f93cc129ad5ca2392880af799c6630c0b5
Matthew Harrington <mattharrington576@gmail.com>
2016-11-07 02:08:58 -0500

    Router and template component

--- app/scripts/components/template.jsx
+++ app/scripts/components/template.jsx
@@ -3,11 +3,14 @@ var React = require('react');
 var TemplateComponent = React.createClass({
   render: function(){
     return (
-
+      <div className="row">
+        <div className="col-md-12">
+        </div>
+      </div>
     )
   }
 });
 
 module.exports = {
   TemplateComponent: TemplateComponent
-}
+};
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,1 +1,29 @@
-var x;
+var Backbone = require('backbone');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var StoreContainer = require('./components/store.jsx').StoreContainer;
+var CartContainer = require('./components/cart.jsx').CartContainer;
+
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    '': 'index',
+    'cart/': 'cart'
+  },
+
+  // initialize ???
+
+  index: function(){
+    ReactDOM.render(
+      React.createElement(StoreContainer, {router: this}),
+      document.getElementById('app')
+    );
+  },
+
+  cart: function(){
+    ReactDOM.render(
+      React.createElement(CartContainer, {router: this}),
+      document.getElementById('app')
+    );
+  }
+});
